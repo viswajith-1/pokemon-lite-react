9bb0bf520d7d45aaacbc40c4ae7722244849bc92
Viswajith S B <121398392+viswajith-1@users.noreply.github.com>
2025-12-08 02:13:30 +0530

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white py-4 mt-8">
         <div className="container mx-auto text-center">
-        <p className="text-sm">&copy; Pokedex Lite - Assignment </p>
+        <p className="text-sm">&copy; Pokedex Lite - Deepsolv Assignment </p>
       </div>
     </footer>
   );
